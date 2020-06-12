19c2736a3c1bac4b69e1410b4df89104c1ac2941
lerato <leratom@commandquality.co.za>
2020-06-12 16:02:48 +0200

    restructuring the ExtentReporting to drive data from a external properties file.

--- suiteFull/src/main/resources/utility/ExtentReporterNg.java
+++ suiteFull/src/main/resources/utility/ExtentReporterNg.java
@@ -1,5 +1,11 @@
 package utility;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
@@ -9,15 +15,43 @@ public class ExtentReporterNg {
 	
 	public static ExtentReports  getReportObject()
 	{
+		String reportName=null;
+		String documentTitle=null;
+		String employeeTitle=null;
+		String employeeName=null;
+		
+		Properties properties = new Properties();
+		FileInputStream input=null;
+		
+		try {
+			 input = new FileInputStream(new File(
+					System.getProperty("user.dir") + "/src/main/resources/com/TestFrame/suiteFull/externalReportData.properties"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			properties.load(input);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		reportName=System.getProperty("ReportName"); 
+		documentTitle=System.getProperty("DocumentTitle"); 
+		employeeTitle=System.getProperty("EmployeeTitle");
+		employeeName=System.getProperty("EmployeeName");
+		
 		String path = System.getProperty("user.dir")+"/reports/index.html";
 		ExtentSparkReporter reporter= new ExtentSparkReporter(path);
 		
-		reporter.config().setReportName("Web Automation Results");
-		reporter.config().setDocumentTitle("Test Results");
+		reporter.config().setReportName(reportName);
+		reporter.config().setDocumentTitle(documentTitle);
 		
 		extent = new ExtentReports();
 		extent.attachReporter(reporter);
-		extent.setSystemInfo("Tester", "Lerato Malatji");
+		extent.setSystemInfo(employeeTitle, employeeName);
 		
 		
 		return extent;
